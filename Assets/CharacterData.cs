e17ee1612867cafd03c1a1a78b23f7e82931d6fe
Fathan Hidayatullah <fathanhidayatullah.2020@student.uny.ac.id>
2023-06-11 08:22:02 +0700

    Merge

--- Assets/CharacterData.cs
+++ Assets/CharacterData.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Character Data", menuName = "ScriptableObjects/CharacterData")]
 public class CharacterData : ScriptableObject
 {
     public Sprite charSprite;
-    public AnimatorController animatorController;
+    public RuntimeAnimatorController animatorController;
 }
