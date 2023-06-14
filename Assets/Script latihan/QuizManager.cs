e69c9e62c626d9998eaaa9a1a6871b470a744f3b
fathanh004 <fathanhidayatullah.2020@student.uny.ac.id>
2023-06-14 16:42:09 +0700

    On Progress Quiz Manager

--- Assets/Script latihan/QuizManager.cs
+++ Assets/Script latihan/QuizManager.cs
@@ -5,73 +5,87 @@ using TMPro;
 public class QuizManager : MonoBehaviour
 {
     public TextMeshProUGUI soalTex;
-       public TextMeshProUGUI judulKuis;
-       public SoalJawab[] soaljawabArray;
+    public SoalJawab[] soaljawabArray;
 
-       public string[] pilihanGanda;
-        public TextMeshProUGUI[] pilGandaUitx;
-       public int indexSoal=0;
-       public int kunciJawabanSekarang;
-       public GameObject benarSalahUi;
-         public GameObject kuisSelesaiUi;
-              public GameObject buttnRetry;
-            public TextMeshProUGUI kuisSelesaiTx;
-       public int maxJumlahSoal=3;
-       public int skorBenar=0;
+    public string[] pilihanGanda;
+    public TextMeshProUGUI[] pilGandaUitx;
+    public int indexSoal = 0;
+    public int kunciJawabanSekarang;
+    public GameObject benarSalahUi;
+    public GameObject kuisSelesaiUi;
+    public GameObject buttnRetry;
+    public TextMeshProUGUI kuisSelesaiTx;
+    public int maxJumlahSoal = 3;
+    public int skorBenar = 0;
 
-         public TextMeshProUGUI benarSalahTx;
+    public TextMeshProUGUI benarSalahTx;
     // Start is called before the first frame update
     void Start()
     {
         GenerateSoal();
     }
-    void GenerateSoal(){
+    void GenerateSoal()
+    {
         soalTex.text = soaljawabArray[indexSoal].soal;
         pilihanGanda = soaljawabArray[indexSoal].pilihanGanda;
-        kunciJawabanSekarang=soaljawabArray[indexSoal].kuncijawaban;
-        for(int i=0;i< pilihanGanda.Length;i++){
-         pilGandaUitx[i].text=  pilihanGanda[i];
+        kunciJawabanSekarang = soaljawabArray[indexSoal].kuncijawaban;
+        for (int i = 0; i < pilihanGanda.Length; i++)
+        {
+            pilGandaUitx[i].text = pilihanGanda[i];
         }
     }
-    public void KlikPilihanGanda(int indexJawab){
+    public void KlikPilihanGanda(int indexJawab)
+    {
         benarSalahUi.gameObject.SetActive(true);
         indexSoal++;
-        if(indexJawab == kunciJawabanSekarang){
-            benarSalahTx.text="Jawaban Benar!";
+        if (indexJawab == kunciJawabanSekarang)
+        {
+            benarSalahTx.text = "Jawaban Benar!";
             skorBenar++;
             //benar
-        }else{
+        }
+        else
+        {
             //salah
-            
-            benarSalahTx.text="Jawaban Salah!";
+
+            benarSalahTx.text = "Jawaban Salah!";
         }
     }
 
-    public void SetelahMenjawab(){
+    public void SetelahMenjawab()
+    {
 
-        if(indexSoal < maxJumlahSoal){
+        if (indexSoal < maxJumlahSoal)
+        {
             //next soal
             GenerateSoal();
-        }else{
+            benarSalahUi.SetActive(false);
+        }
+        else
+        {
             kuisSelesaiUi.SetActive(true);
-            if(skorBenar == maxJumlahSoal){
-                kuisSelesaiTx.text="Selamat Kuis Selesai dengan jawaban Benar semua!";
+            if (skorBenar == maxJumlahSoal)
+            {
+                kuisSelesaiTx.text = "Selamat Kuis Selesai dengan jawaban Benar semua!";
                 buttnRetry.SetActive(false);
                 //dapat hadiah/menang
                 // SistemInventori.instance.TambahItem(GameManagerLatihan.instance.hadiahInventori[0]);
-            }else{
-                 kuisSelesaiTx.text="Maaf terdapat soal dengan jawaban yang salah, coba lagi?";
-                 buttnRetry.SetActive(true);
+            }
+            else
+            {
+                kuisSelesaiTx.text = "Maaf terdapat soal dengan jawaban yang salah, coba lagi?";
+                buttnRetry.SetActive(true);
             }
             //Kuis selesai
         }
     }
-    public void Retry(){
-         kuisSelesaiUi.SetActive(false);
-      benarSalahUi.SetActive(false);  
-        indexSoal=0;
-        skorBenar=0;
+    public void Retry()
+    {
+        kuisSelesaiUi.SetActive(false);
+        benarSalahUi.SetActive(false);
+        indexSoal = 0;
+        skorBenar = 0;
         GenerateSoal();
     }
-   
+
 }
